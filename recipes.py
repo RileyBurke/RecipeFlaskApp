25648c1cfeb7e41aab6b1af4c464492effc3d310
Riley Burke <rileyburke0@hotmail.com>
2022-04-05 21:31:25 -0230

    Started work on login system, database and javascript verification

--- recipes.py
+++ recipes.py
@@ -1,10 +1,57 @@
 import os
 import csv
-from flask import Flask, render_template, request, redirect, url_for
-import flask_login
+from flask import Flask, render_template, request, redirect, url_for, Response
+from flask_login import LoginManager, login_required, login_user, logout_user, current_user, UserMixin
 import sqlite3
 
 app = Flask(__name__)
+login_manager = LoginManager()
+recipe_app_database = "recipesRB.db"
+
+
+# class User(UserMixin):
+#     def __init__(self, username, password):
+#         self._username = # username column
+#         self._password = # password column
+#         self._authenticated =
+#
+#     @property
+#     def is_active(self):
+#         return True
+#
+#     @property
+#     def get_username(self):
+#         return self._username
+#
+#     @property
+#     def is_authenticated(self):
+#         return self._authenticated
+#
+#     def is_anonymous(self):
+#         return False
+
+# @login_manager.user_loader
+# def user_loader(username):
+#     return User.query.get
+
+
+def create_database():
+    conn = sqlite3.connect('recipesRB.db')
+    conn.execute('CREATE TABLE users (name TEXT, password TEXT)')
+    print("Table created successfully")
+    conn.close()
+
+
+def create_new_user(username, password):
+    database_connection = connect_to_database()
+    cursor = database_connection.cursor()
+    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+    database_connection.commit()
+    database_connection.close()
+
+
+def connect_to_database():
+    return sqlite3.connect(recipe_app_database)
 
 
 @app.route("/")
@@ -18,28 +65,37 @@ def profile(username):
 
 
 @app.route("/user/<string:username>/upload")
+@login_required
 def upload(username):
     return render_template("upload.html")
 
 
 @app.route("/user/<string:username>/remove")
+@login_required
 def remove(username):
     return render_template("remove.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    return render_template("login.html", mode="login")
 
 
 @app.route("/logout")
+@login_required
 def logout():
     return "SIGN OUT"
 
 
-@app.route("/signup")
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
-    return render_template("signup.html")
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        password_reentry = request.form['password2']
+        if 16 >= len(username) > 4 and 20 >= len(password) > 8 and password == password_reentry:
+            create_new_user(username, password)
+    return render_template("login.html", mode="signup")
 
 
 @app.route("/category/<string:category>")
@@ -55,3 +111,6 @@ def recipe_list(category):
 @app.route("/category/<string:category>/<string:recipe>")
 def view_recipe(category, recipe):
     return render_template("recipe.html", category=category, recipe=recipe)
+
+
+
--- upload_verification.js
+++ upload_verification.js
@@ -2,17 +2,41 @@
 
 const $ = selector => document.querySelector(selector);
 
-const validExtensions = ["jpg", "jpeg", "png", "bmp"];
+$("#submit_recipe").addEventListener("click", submitRecipe);
 
-let isValidFile;
-let fileSplit = $("image_upload").textContent.split(".");
-let fileExtension = fileSplit[-1].toLowerCase();
+function submitRecipe() {
+    const validExtensions = ["jpg", "jpeg", "png", "bmp"];
+    let isValidFile;
+    let fileSplit = $("image_upload").textContent.split(".");
+    let fileExtension = fileSplit[-1].toLowerCase();
 
+    if (!(fileExtension in validExtensions)){
+        isValidFile = false;
+        $("#image_upload").nextElementSibling.textContent = "Invalid image file. Must use a jpg, png, or bmp file.";
+    }else if($("#image_upload").value() === null){
+        isValidFile = false;
+        $("").nextElementSibling.textContent = "Must include an image file.";
+    } else{
+        isValidFile = true;
+    }
 
-if (!(fileExtension in validExtensions)){
-    isValidFile = false;
-    $("image_upload").nextElementSibling.textContent = "Invalid image file.";
-}else{
-    isValidFile = true;
+    if ($("#serving_size").value() === ""){
+        isValidFile = false;
+        $("").nextElementSibling.textContent = "Must include a serving size.";
+    }
+
+    if ($("#ingredients").value() === "") {
+        isValidFile = false;
+        $("").nextElementSibling.textContent = "Must include a list of ingredients.";
+    }
+
+    if ($("#instructions").value() === "") {
+        isValidFile = false;
+        $("").nextElementSibling.textContent = "Must include cooking instructions.";
+    }
+
+    if (isValidFile){
+        $("#submit_recipe").submit();
+    }
 }
 
